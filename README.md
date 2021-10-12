831bafeaa083c2b0d766a1fa185c0b92cd7043fa
VicenzoMantilla <vicenzomantilla94@gmail.com>
2021-10-11 23:12:10 -0300

    last function 6-e added

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 ## RR COURSE- UPDATE - WEEK - 04
-
-## Description
-
-
+    This week's problem has been solved using Javascript.
+## Javascript
+    The first objetive for this week was to solve a series of exercises using
+    Javascript.
+* These exercises were divided in 6 cathegories:
+    * Variables and operators
+    * Strings
+    * Arrays
+    * If Else conditions
+    * For loops
+    * Functions
+* You'll find an index.html file that let you see the results for the excercises in the browser's console.
 
 ## Get Started
-As well as the last week, you can donwload the proyect from this repository and open the 'index.html' file on your browser or you can access by clicking on this link [GitHubPage](https://vicenzomantilla.github.io/RR-DEV-VM-04/)
+As well as the last week, you can donwload the proyect from this repository and open the 'index.html' file on your browser or you can access by clicking on this link [GitHubPage](https://vicenzomantilla.github.io/RR-DEV-VM-04/).
+
+## Last week feedback.
+
+#### Corrections made
+
+* HTML clases inconcistency.
+* iframe tag removal and replace it for a video.
+* CSS unnecessary spaces.
+* Media-queries order.
+
+If you want to check the last week project you can do it by clicking [GitHubPage](https://vicenzomantilla.github.io/RR-DEV-VM-03/).
--- exercises/functions.js
+++ exercises/functions.js
@@ -29,19 +29,36 @@ function validateInteger(number){
 }
 console.log(validateInteger(number));
 /* ---- ---- */
-function validate(a) {
-    return Number.isInteger(a);
-}
-
 function sum(number1,number2) {
     if (isNaN(number1) || isNaN(number2)){
         alert("This is not a number, try again");
         return NaN;
-    }else if (validate(number1)!== true){
+    }else if (validateInteger(number1)!== true){
+        alert(number1 + "This is not an Integer number, try again");
+        a = Math.round(number1);
+        return a
+    }else if (validateInteger(number2)!== true){
+        alert(number2 + "This is not an Integer number, try again");
+        b = Math.round(number2);
+        return b
+    }else{
+        return number1 + number2
+    }
+}
+console.log(sum('2df',23));
+console.log(sum(2.9,10));
+console.log(sum(10,10.5));
+console.log(sum(10,20));
+/* ---- ---- */
+function validateComplete(number1,number2){
+    if (isNaN(number1) || isNaN(number2)){
+        alert("This is not a number, try again");
+        return NaN;
+    }else if (validateInteger(number1)!== true){
         alert(number1 + "This is not an Integer number, try again");
         a = Math.floor(number1);
         return a
-    }else if (validate(number2)!== true){
+    }else if (validateInteger(number2)!== true){
         alert(number2 + "This is not an Integer number, try again");
         b = Math.floor(number2);
         return b
@@ -49,9 +66,9 @@ function sum(number1,number2) {
         return number1 + number2
     }
 }
-console.log(sum('2df',23))
-console.log(sum(2.9,10));
-console.log(sum(10,10.5));
-console.log(sum(10,20));
 
-/* ---- ---- */
+function sum(number1,number2){
+    return validateComplete(number1,number2);
+}
+
+console.log(sum(3,'qwert'));
